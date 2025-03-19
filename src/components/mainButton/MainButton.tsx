95aa2a2d37d991fe5e8e032e4389b9d51a024cea
KiBohr <katharinabohr@web.de>
2025-03-19 16:02:40 +0100

    mainButton changed, startPage styled

--- src/components/mainButton/MainButton.tsx
+++ src/components/mainButton/MainButton.tsx
@@ -2,13 +2,13 @@ import { Link } from "react-router-dom";
 
 interface IMainButtonProps{
     buttonText: string,
-    path: {},
+    path?: {},
 }
 
 const MainButton = ({buttonText, path}:IMainButtonProps) => {
     return ( 
     <>
-        <Link className="border-red border-2 px-3 py-2 text-white transition ease-in-out hover:opacity-75" to={path} >{buttonText}</Link>
+        <Link className="border-red bg-red rounded-lg border-2 px-8 py-2 mb-5 text-white transition ease-in-out hover:opacity-75" to={path} >{buttonText}</Link>
     </> 
     );
 }
--- src/pages/startPage/StartPage.tsx
+++ src/pages/startPage/StartPage.tsx
@@ -3,23 +3,25 @@ import { Home } from "../home/Home";
 
 const StartPage = () => {
     return ( 
-        <section>
-            <div>
-                <img src="../../../intro-img.png" alt="Screenshots of the app" />
+        <section className="flex flex-col items-center gap-4 font-[Poppins]">
+            <div className="h-110 w-[90%] overflow-hidden bg-linear-to-bl from-red/50 to-white rounded-t-4xl pt-10 px-5 mb-5">
+                <img className="object-cover" src="../../../intro-img.png" alt="Screenshots of the app" />
             </div>
 
-            <div>
+            <div className="flex flex-col items-center text-2xl font-bold">
                 <h2>Enjoy Your Movie</h2>
                 <h2>Watch Everywhere</h2>
             </div>
 
-            <p>Stream unlimited movies and TV showson your phone,tablet, laptop, and TV.</p>
+            <p className="w-[85%] text-center">Stream unlimited movies and TV showson your phone,tablet, laptop, and TV.</p>
 
-            {/* <MainButton buttonText="Get Started" path={<Home/>}/> */}
+            <MainButton buttonText="Get Started" />
 
-            <a href="https://developer.themoviedb.org/docs/getting-started">
-            <img src="../../../public/tmd-logo.svg" alt="logo of TMDB" />
+            <a className="h-10 w-15" href="https://developer.themoviedb.org/docs/getting-started">
+            <img className="object-cover" src="../../../public/tmd-logo.svg" alt="logo of TMDB" />
             </a>
+
+        
         </section>
      );
 }
