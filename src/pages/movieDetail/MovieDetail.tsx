db69704f4664beced375035449f29927f8d1cd55
DeborahAleaKoch <alea-de@web.de>
2025-03-20 17:53:13 +0100

    Update MovieDetail.tsx

--- src/pages/movieDetail/MovieDetail.tsx
+++ src/pages/movieDetail/MovieDetail.tsx
@@ -5,10 +5,16 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { MovieDetails } from "../contracts/interfaces";
+import { MovieDetails } from "../../contracts/interfaces";
 
 export const MovieDetail = () => {
 	const { movieParam } = useParams();
+
+	let [showOverview, setShowOverview] = useState<boolean>(false);
+
+	const toggleOverview = () => {
+		setShowOverview(!showOverview);
+	};
 
 	const [movieItem, setMovieItem] = useState<MovieDetails>();
 
@@ -38,5 +44,57 @@ export const MovieDetail = () => {
 		fetchMovieData();
 	}, []);
 
-	return <>{movieItem}</>;
+	if (!movieItem) {
+		return <div>loading…</div>;
+	}
+
+	return (
+		<div>
+			<div>
+				<img
+					src={`https://image.tmdb.org/t/p/w500${movieItem.poster_path}`}
+					alt={movieItem.title}
+				/>
+			</div>
+			<div>
+				<h3>Movie Details</h3>
+				<p>{movieItem.title}</p>
+				<div>
+					<p>⭐️{movieItem.vote_average}</p>
+					<p>{movieItem.release_date}</p>
+					<p>{movieItem.genres[0].name}</p>
+					<p>{movieItem.runtime}</p>
+				</div>
+
+				<h1>Overview</h1>
+				<p>
+					{movieItem.overview.length > 100 && !showOverview
+						? movieItem.overview.slice(0, 100) + "..."
+						: movieItem.overview}
+				</p>
+				<a onClick={toggleOverview} className='text-main-red'>
+					{showOverview ? " See less..." : " See more..."}
+				</a>
+
+				<div>
+					<p>Genres</p>
+					<div>
+						{movieItem.genres.map((genre) => (
+							<p>{genre.name}</p>
+						))}
+					</div>
+				</div>
+				<div>
+					<p>Languages</p>
+					<div>
+						{movieItem.spoken_languages.map((language) => (
+							<p>{language.name}</p>
+						))}
+					</div>
+				</div>
+			</div>
+
+			{/* <MainButton buttonText="Trailer" to={}/> */}
+		</div>
+	);
 };
